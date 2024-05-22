bd06bbb32a5d725d883f9eb07d95a66a1bb2fd14
osg9010 <osg3419010@naver.com>
2024-05-22 10:05:51 +0900

    서블릿 예외처리 1. 뷰 템플릿 resources/templates/error/500.html resources/templates/error/5xx.html 2. 정적 리소스( static , public ) resources/static/error/400.html resources/static/error/404.html resources/static/error/4xx.html 3. 적용 대상이 없을 때 뷰 이름( error ) resources/templates/error.html
    해당 경로 위치에 HTTP 상태 코드 이름의 뷰 파일을 넣어두면 자동으로 적용된다.

--- exception/src/main/java/hello/exception/servlet/ServletExceptionController.java
+++ exception/src/main/java/hello/exception/servlet/ServletExceptionController.java
@@ -20,6 +20,10 @@ public class ServletExceptionController {
     public void error404(HttpServletResponse response) throws IOException {
         response.sendError(404,"404오류!");
     }
+    @GetMapping("/error-409")
+    public void error409(HttpServletResponse response) throws IOException {
+        response.sendError(409,"404오류!");
+    }
     @GetMapping("/error-500")
     public void error500(HttpServletResponse response) throws IOException {
         response.sendError(500);
