bc77e0b78763a463c99c7da82790d7bc37b4484b
osg9010 <osg3419010@naver.com>
2024-05-24 10:32:59 +0900

    UserHandlerExceptionResolver 를 통해 API UserException 에러처리

--- exception/src/main/java/hello/exception/api/ApiExceptionController.java
+++ exception/src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,5 +1,6 @@
 package hello.exception.api;
 
+import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,9 @@ public class ApiExceptionController {
         }
         if(id.equals("bad")){
             throw new IllegalArgumentException("잘못된 입력 값");
+        }
+        if(id.equals("user-ex")){
+            throw new UserException("사용자오류");
         }
         return new MemberDto(id,"hello "+id);
     }
--- exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
+++ exception/src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -11,17 +11,18 @@ import java.io.IOException;
 @Slf4j
 public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
     @Override
-    public ModelAndView resolveException(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+    public ModelAndView resolveException(HttpServletRequest request,
+                                         HttpServletResponse response, Object handler, Exception ex) {
         try {
             if (ex instanceof IllegalArgumentException) {
                 log.info("IllegalArgumentException resolver to 400");
-                response.sendError(HttpServletResponse.SC_BAD_REQUEST, ex.getMessage());
+                response.sendError(HttpServletResponse.SC_BAD_REQUEST,
+                        ex.getMessage());
                 return new ModelAndView();
             }
         } catch (IOException e) {
-            log.error("resolver ex");
+            log.error("resolver ex", e);
         }
-
         return null;
     }
 }
